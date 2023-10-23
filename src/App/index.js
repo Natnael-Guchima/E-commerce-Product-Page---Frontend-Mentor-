aebd39c43f4eae261ab11edb44f038cee52d7383
Natnael-Guchima <57330233+Natnael-Guchima@users.noreply.github.com>
2023-10-23 22:47:35 +0300

    Update index.js

--- src/App/index.js
+++ src/App/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ProductCarousal from "../layouts/ProductCarousal/ProductCarousal";
 
 import ProductDetails from "../layouts/ProductDetails";
-
+//comment test
 const App = () => {
   return (
     <div className="flex flex-col">
